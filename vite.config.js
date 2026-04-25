3995be5ff6d3f14711e49d2a71a6ca58e8e2bc69
tuansonhq <105621622+tuanson1985@users.noreply.github.com>
2026-04-25 23:27:09 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/CV2026/' : '/',
   plugins: [react()],
   build: {
     // ESNext → output nhỏ nhất, không polyfill cũ
